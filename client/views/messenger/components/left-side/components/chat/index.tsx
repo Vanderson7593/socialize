e041b4dae6e570d7b6194574af9646eb20310350
Vanderson Telema <vandelson14@hotmail.com>
2021-08-27 23:27:52 +0100

    chat item alignment

--- client/views/messenger/components/left-side/components/chat/index.tsx
+++ client/views/messenger/components/left-side/components/chat/index.tsx
@@ -10,14 +10,14 @@ const Chat: FC<{ isReaded: boolean; c: boolean }> = ({ isReaded, c }) => {
   return (
     <View style={{ cursor: 'pointer' }}>
       <Flex>
-        <Flex style={{ gap: 10 }}>
+        <Flex flex={1} style={{ gap: 10 }}>
           <Avatar src="/van.jpg" width={70} height={70} />
-          <Flex flexDirection="column" style={{ gap: 4 }} width={279}>
+          <Flex flexDirection="column" style={{ gap: 4 }} width={306}>
             <Heading color={colors.BLUE}>Vanderson Telema</Heading>
             <Text>Nós estamos usando, styled-system e @emotion styled!</Text>
           </Flex>
         </Flex>
-        <Flex position="relative">
+        <Flex flex={2} justifyContent="flex-end" position="relative">
           <Text>10:34 AM</Text>
           <View position="absolute" top={20} right={0}>
             {isReaded ? <Badge>1</Badge> : c ? <Check /> : <Check stroke={colors.BLUE} />}
