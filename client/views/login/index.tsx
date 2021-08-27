b5784e0fe9b104a5c216e446d1acaa3e5e3f5605
Vanderson Telema <vandelson14@hotmail.com>
2021-08-27 23:52:30 +0100

    create login view

---
+++ client/views/login/index.tsx
@@ -0,0 +1,13 @@
+import { Flex } from '@elements'
+import { useTheme } from '@emotion/react'
+import { FC } from 'react'
+
+const Login: FC = () => {
+  const { colors } = useTheme()
+  return (
+    <Flex>
+      <Flex />
+    </Flex>
+  )
+}
+export default Login
