df4015270ed07215af42c8cde2fe635eeef7ae71
Vanderson Telema <vandelson14@hotmail.com>
2021-08-28 03:02:19 +0100

    set up register done

---
+++ client/views/register/index.tsx
@@ -0,0 +1,41 @@
+import { Input } from '@components'
+import { Flex, Heading, Text, View } from '@elements'
+import { useTheme } from '@emotion/react'
+import { ArrowRightCircle } from '@svgs'
+import { FC } from 'react'
+import Link from 'next/link'
+
+const Register: FC = () => {
+  const { colors } = useTheme()
+  return (
+    <Flex
+      style={{ gap: 30 }}
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      height="100vh"
+    >
+      <Heading textAlign="center" color={colors.BLUE} as="h3">
+        Welcome to socialize <br />
+        Login to start texting!
+      </Heading>
+      <View>
+        <Text>Username</Text>
+        <Input placeholder="Username" bgColor={colors.FOREGROUND} />
+      </View>
+      <View mt="-28px">
+        <Text>Password</Text>
+        <Input placeholder="Password" bgColor={colors.FOREGROUND} />
+      </View>
+      <View>
+        <ArrowRightCircle />
+      </View>
+      <View style={{ cursor: 'pointer' }}>
+        <Link href="#register">
+          <Heading color={colors.BLUE}>Create new account</Heading>
+        </Link>
+      </View>
+    </Flex>
+  )
+}
+export default Register
