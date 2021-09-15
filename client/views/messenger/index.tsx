c230fbb12f465cb2c92c67c10383dfbe0c201a6c
Vanderson Telema <vandelson14@hotmail.com>
2021-09-15 19:55:09 +0100

    chore: socket io

--- client/views/messenger/index.tsx
+++ client/views/messenger/index.tsx
@@ -1,19 +1,26 @@
 import { Flex, View } from '@elements'
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
+import SocketIO, { Socket } from 'socket.io-client'
 import LeftSide from './components/left-side'
 import Middle from './components/middle'
 
-const Messenger: FC = () => (
-  <View>
-    <Flex
-      flexDirection={['column', 'column', 'column', 'row']}
-      alignItems={['center', 'center', 'center', 'flex-start']}
-      justifyContent={['center', 'center', 'center', 'normal']}
-    >
-      <LeftSide />
-      <Middle />
-    </Flex>
-  </View>
-)
+const Messenger: FC = () => {
+  useEffect(() => {
+    SocketIO()
+  }, [])
+
+  return (
+    <View>
+      <Flex
+        flexDirection={['column', 'column', 'column', 'row']}
+        alignItems={['center', 'center', 'center', 'flex-start']}
+        justifyContent={['center', 'center', 'center', 'normal']}
+      >
+        <LeftSide />
+        <Middle />
+      </Flex>
+    </View>
+  )
+}
 
 export default Messenger
