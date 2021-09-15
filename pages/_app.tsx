b6b3bc96d072215239ebf4d632b6cd3f6ed758e7
Vanderson Telema <vandelson14@hotmail.com>
2021-09-16 00:40:47 +0100

    refactor: socket context to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,16 @@ import { Theme, theme } from 'client/design/theme'
 import { Global, ThemeProvider } from '@emotion/react'
 import { GlobalStyles } from 'client/design/styles'
 import type { AppProps } from 'next/app'
+import SocketProviderContext from 'client/context/socket-context'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Global styles={GlobalStyles} />
       <ThemeProvider theme={theme as Theme}>
-        <Component {...pageProps} />
+        <SocketProviderContext>
+          <Component {...pageProps} />
+        </SocketProviderContext>
       </ThemeProvider>
     </>
   )
